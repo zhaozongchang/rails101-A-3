f668bdb931a604bdc03acf326c9eff875c7a2aef
zhaozongchang <xingyingsuifeng@gmail.com>
2018-05-13 22:13:38 +0800

    user before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -25,11 +26,11 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+
   end
 
   def update
-    @group = Group.find(params[:id])
+
     if @group.update(group_params)
     redirect_to groups_path, notice: "更新成功"
   else
@@ -38,12 +39,19 @@ class GroupsController < ApplicationController
   end
 
    def destroy
-     @group = Group.find(params[:id])
+     
      @group.destroy
      redirect_to groups_path, alert: "删除成功"
    end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "你有权限"
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
