730e0ad7864caf1d3306ec1e8f351b7b354738ba
zhaozongchang <xingyingsuifeng@gmail.com>
2018-05-14 21:35:18 +0800

    implement is_member_of?(group)

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,8 @@ class Group < ApplicationRecord
   validates :title, presence: true
 
   belongs_to :user
-  has_many :posts 
+  has_many :posts
+
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
     has_many :groups
-    has_many :posts 
+    has_many :posts
+
+    has_many :group_relationships
+    has_many :participated_groups, :through => :group_relationships, :source => :group
+
+    def is_member_of?(group)
+      participated_groups.include?(group)
+    end 
 end
